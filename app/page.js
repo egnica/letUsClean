2e8325dee1d35f71f7e9870a30f78fad6012e0f4
Nicholas Egner <nick@nicholasegner.com>
2025-10-14 11:15:44 -0500

    image added

--- app/page.js
+++ app/page.js
@@ -5,13 +5,19 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <header>
+        <Image
+          alt="let us clean logo"
+          height={140}
+          width={140}
+          src="https://nciholasegner.s3.us-east-2.amazonaws.com/let-us-clean/clean-logo.webp"
+        />
         <h1>Let Us Clean MN</h1>
         <p>Home</p>
         <p>About</p>
       </header>
-    <main>
-      <p>test</p>
-    </main>
+      <main>
+        <p>test</p>
+      </main>
     </div>
   );
 }
