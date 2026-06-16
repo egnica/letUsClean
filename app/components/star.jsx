22223129ad3afe8e5ee3b6fcce7c5b13943106b2
Nicholas Egner <nick@nicholasegner.com>
2026-06-16 08:50:18 -0500

    google image alt

--- app/components/star.jsx
+++ app/components/star.jsx
@@ -4,6 +4,7 @@ function star() {
   return (
     <img
       width={30}
+      alt="google 5 star"
       src={
         "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABUAAAATCAYAAAB/TkaLAAAAmElEQVR4AWLwySwkiL9vZwMUTwcWAMQwFEDnyESdJKt0lEyUdY479MMnl3+Ew6fQ1yZpHWslCmhPric2ifpBfRKNg6aOaqUjNoE6oT6BBqEhodg4kABalSmkbgsPJT9iycMDqsECyCjjuwE37/kNza50FQW4xJ6uCpUe/DlI/gjdX19NFaagjlsUG3AwBukKut96xbeuXsEN4D2kcYB5knEAAAAASUVORK5CYII="
       }
